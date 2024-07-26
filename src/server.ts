61aaff3fea752f5409975da85782e19b29683fb2
Nguyễn Phan Vũ <npvu1510@gmail.com>
2024-07-26 17:13:55 +0700

    thiết lập dự án với nodemon ts-node

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv'
+dotenv.config({ path: '.env' })
+
+import app from './app'
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`)
+})
